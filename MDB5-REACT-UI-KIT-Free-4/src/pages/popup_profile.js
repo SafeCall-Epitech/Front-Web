a20694eba5d4045158ad6de0362950527c9fbe86
Morton Sean <morton.sean@icloud.com>
2023-05-25 23:34:27 +0900

    [FIX] deleted the form on the wrong page

--- MDB5-REACT-UI-KIT-Free-4/src/pages/popup_profile.js
+++ MDB5-REACT-UI-KIT-Free-4/src/pages/popup_profile.js
@@ -58,59 +58,59 @@ export default function ECommerce() {
                   <MDBModalBody>
                     <MDBCard style={{ borderRadius: "15px", backgroundColor: "#E6E6E6" }}>
                       <MDBCardBody className="p-4 text-black">
-                <div>
+                        <div>
 
-                {Load ?  <MDBTypography tag="h5">No Name</MDBTypography>
-                    :
-                    <MDBTypography tag="h5">{Name}</MDBTypography>
-                  } 
-                <div className="d-flex align-items-center justify-content-between mb-3">
-                    <p className="small mb-0"><MDBIcon far icon="clock me-2" />6 hrs ago</p>
-                  </div>
-                </div>
-                <div className="d-flex align-items-center mb-4">
-                  <div className="flex-shrink-0">
-                    <MDBCardImage
-                      style={{ width: '70px' }}
-                      className="img-fluid rounded-circle border border-dark border-3"
-                      src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
-                      alt='Generic placeholder image'
-                      fluid />
-                  </div>
-                  <div className="flex-grow-1 ms-3">
-                    <div className="d-flex flex-row align-items-center mb-2">
-                      <p className="mb-0 me-2">@ID</p>
-                      <ul className="mb-0 list-unstyled d-flex flex-row" style={{ color: '#1B7B2C' }}>
-                        <li>
-                          <MDBIcon fas icon="star fa-xs" />
-                        </li>
-                        <li>
-                          <MDBIcon fas icon="star fa-xs" />
-                        </li>
-                        <li>
-                          <MDBIcon fas icon="star fa-xs" />
-                        </li>
-                        <li>
-                          <MDBIcon fas icon="star fa-xs" />
-                        </li>
-                        <li>
-                          <MDBIcon fas icon="star fa-xs" />
-                        </li>
-                      </ul>
+                          {Load ? <MDBTypography tag="h5">No Name</MDBTypography>
+                            :
+                            <MDBTypography tag="h5">{Name}</MDBTypography>
+                          }
+                          <div className="d-flex align-items-center justify-content-between mb-3">
+                            <p className="small mb-0"><MDBIcon far icon="clock me-2" />6 hrs ago</p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center mb-4">
+                          <div className="flex-shrink-0">
+                            <MDBCardImage
+                              style={{ width: '70px' }}
+                              className="img-fluid rounded-circle border border-dark border-3"
+                              src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
+                              alt='Generic placeholder image'
+                              fluid />
+                          </div>
+                          <div className="flex-grow-1 ms-3">
+                            <div className="d-flex flex-row align-items-center mb-2">
+                              <p className="mb-0 me-2">@ID</p>
+                              <ul className="mb-0 list-unstyled d-flex flex-row" style={{ color: '#1B7B2C' }}>
+                                <li>
+                                  <MDBIcon fas icon="star fa-xs" />
+                                </li>
+                                <li>
+                                  <MDBIcon fas icon="star fa-xs" />
+                                </li>
+                                <li>
+                                  <MDBIcon fas icon="star fa-xs" />
+                                </li>
+                                <li>
+                                  <MDBIcon fas icon="star fa-xs" />
+                                </li>
+                                <li>
+                                  <MDBIcon fas icon="star fa-xs" />
+                                </li>
+                              </ul>
 
-                    </div>
-                    <div>
-                      <MDBBtn outline color="dark" rounded size="sm">- DELETE FRIEND</MDBBtn>
-                      <MDBBtn outline color="dark" rounded size="sm" className="mx-1">profile</MDBBtn>
-                      <MDBBtn outline color="dark" floating size="sm"><MDBIcon fas icon="comment" /></MDBBtn>
-                    </div>
-                  </div>
-                </div>
-                <hr />
-                <MDBBtn color="dark" rounded block size="lg">
-                  <MDBIcon far icon="clock me-2" /> Book a call
-                </MDBBtn>
-              </MDBCardBody>
+                            </div>
+                            <div>
+                              <MDBBtn outline color="dark" rounded size="sm">- DELETE FRIEND</MDBBtn>
+                              <MDBBtn outline color="dark" rounded size="sm" className="mx-1">profile</MDBBtn>
+                              <MDBBtn outline color="dark" floating size="sm"><MDBIcon fas icon="comment" /></MDBBtn>
+                            </div>
+                          </div>
+                        </div>
+                        <hr />
+                        <MDBBtn color="dark" rounded block size="lg">
+                          <MDBIcon far icon="clock me-2" /> Book a call
+                        </MDBBtn>
+                      </MDBCardBody>
                     </MDBCard>
                   </MDBModalBody>
                 </MDBModalContent>
@@ -123,4 +123,3 @@ export default function ECommerce() {
   );
 }
 
-              
