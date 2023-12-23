ff34d37f594b5abc9da36a1e2c9dc93c4aa4cbd8
Morton Sean <morton.sean@icloud.com>
2023-12-23 15:06:47 +0100

    [ADD] added some specs on the cal buttons

--- src/pages/call_popup.js
+++ src/pages/call_popup.js
@@ -1,30 +1,22 @@
-import React, {useEffect, useRef, useState} from "react"
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import {
   MDBContainer,
   MDBRow,
   MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBTypography,
-  MDBIcon,
-  MDBBtn,
   MDBModal,
   MDBModalDialog,
   MDBModalContent,
   MDBModalHeader,
   MDBModalBody,
   MDBModalFooter,
-  MDBInput,
+  MDBModalTitle,
+  MDBBtn,
 } from "mdb-react-ui-kit";
+
 import Draggable from "react-draggable";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWindowMinimize, faTimes } from "@fortawesome/free-solid-svg-icons";
-
-import Peer from "simple-peer"
-import io from "socket.io-client"
-import { Input } from "@chakra-ui/react"; 
+import Peer from "simple-peer";
+import io from "socket.io-client";
 
 const socket = io.connect("https://x2024safecall3173801594000.westeurope.cloudapp.azure.com:5000/");
 
@@ -35,14 +27,13 @@ export default function ECommerce() {
   const [Email, setEmail] = useState("");
   const [Nb, setNb] = useState("");
   const [Description, setDescription] = useState("");
-  const [modalShow, setModalShow] = useState(true);
+  const [modalShow, setModalShow] = useState(false);
   const [Load, setLoad] = useState(true);
   const [callDuration, setCallDuration] = useState(0);
   const [isMuted, setIsMuted] = useState(false);
   const [isSoundMuted, setIsSoundMuted] = useState(false);
-
-
-// Call 
+  const [showCallPopup, setShowCallPopup] = useState(false); // Initially set to true
+  const [showCallAnswerModal, setShowCallAnswerModal] = useState(false);
 
   const [me, setMe] = useState("");
   const [stream, setStream] = useState();
@@ -52,11 +43,6 @@ export default function ECommerce() {
   const [callAccepted, setCallAccepted] = useState(false);
   const [idToCall, setIdToCall] = useState("");
   const [callEnded, setCallEnded] = useState(false);
-
-  const [micro, setMicro] = useState(true);
-  const [wasMicroEnabled, setWasMicroEnabled] = useState(true);
-  const [sound, setSound] = useState(true);
-  const [cam, setCam] = useState(true);
 
   const myVideo = useRef();
   const userVideo = useRef();
@@ -100,96 +86,68 @@ export default function ECommerce() {
 
   const answerCall = () => {
     setCallAccepted(true);
+    // setShowCallPopup(false); // Remove this line to keep the call popup open
+    setShowCallAnswerModal(true); // Show the call answer modal
     const peer = new Peer({
       initiator: false,
       trickle: false,
-      stream: stream
+      stream: stream,
     });
-
+  
     peer.on("signal", (data) => {
-      socket.emit("answerCall", {signal: data, to: caller});
+      socket.emit("answerCall", { signal: data, to: caller });
     });
-
+  
     peer.on("stream", (stream) => {
-      stream.getAudioTracks()[0].enabled = sound;
-      if (userVideo.current)
-        userVideo.current.srcObject = stream;
+      // Check if audio should be muted based on isSoundMuted state
+      stream.getAudioTracks()[0].enabled = !isSoundMuted;
+      if (userVideo.current) userVideo.current.srcObject = stream;
     });
-
+  
     socket.on("callEnded", () => {
       leaveCall();
     });
-
+  
     peer.signal(callerSignal);
     connectionRef.current = peer;
   };
   
+
   const leaveCall = () => {
     setCallEnded(true);
     socket.disconnect();
-//    connectionRef.current.destroy();
+    // connectionRef.current.destroy();
   };
 
-  const muteMicro = () => {
-    if (!sound) {
-      muteSound();
-      stream.getAudioTracks()[0].enabled = true;
-      setMicro(true);
-    } else {
-      stream.getAudioTracks()[0].enabled = !micro;
-      setMicro(!micro);
-    }
+  const rejectCall = () => {
+    setShowCallPopup(false);
+    setReceivingCall(false);
+    // Additional logic for rejecting the call
   };
-
-  const cutCam = () => {
-    stream.getVideoTracks()[0].enabled = !cam;
-    setCam(!cam);
-  };
-
-
-  const muteSound = () => {
-    if (!sound && wasMicroEnabled) {
-      stream.getAudioTracks()[0].enabled = true;
-      if (userVideo.current)
-        userVideo.current.srcObject.getAudioTracks()[0].enabled = true;
-      setMicro(true);
-    } else {
-      if (userVideo.current)
-        userVideo.current.srcObject.getAudioTracks()[0].enabled = false;
-      stream.getAudioTracks()[0].enabled = false;
-    }
-    setWasMicroEnabled(micro);
-    if (sound)
-      setMicro(false);
-    setSound(!sound);
-  }
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((stream) => {
-      console.log("UseEffectMedia", stream)
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
       setStream(stream);
-      if (myVideo.current)
+      if (myVideo.current) {
         myVideo.current.srcObject = stream;
+      }
     });
-    console.log("En dehors", myVideo)
+
     socket.on("me", (id) => {
       setMe(id);
     });
 
     socket.on("callUser", (data) => {
-      console.log("UseEffectCallUser", data)
       setReceivingCall(true);
       setCaller(data.from);
       setCallerSignal(data.signal);
+      setShowCallPopup(true); // Show the modal
     });
   }, []);
 
-
   useEffect(() => {
     async function fetchData() {
-      const res = await axios.get(
-        `https://x2024safecall3173801594000.westeurope.cloudapp.azure.com/profile/${user}`
-      );
+      const res = await axios.get(`https://x2024safecall3173801594000.westeurope.cloudapp.azure.com/profile/${user}`);
       setName(res.data["profile"]["FullName"]);
       setEmail(res.data["profile"]["Email"]);
       setNb(res.data["profile"]["PhoneNb"]);
@@ -199,44 +157,63 @@ export default function ECommerce() {
     fetchData();
   }, []);
 
-  // Function to toggle mute status
-  const toggleMute = () => {
-    setIsMuted(!isMuted);
-  };
+  const CallAnswerModal = () => (
+    <MDBModal show={showCallAnswerModal} tabIndex="-1">
+      <MDBModalDialog>
+        <MDBModalContent>
+          <MDBModalHeader>
+            <MDBModalTitle>Call Accepted</MDBModalTitle>
+          </MDBModalHeader>
+          <MDBModalBody>
+            <p>You have accepted the call from {caller}.</p>
+            
+            {/* Place the video container here */}
+            <div className="video-container">
+              <div className="video">
+                {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+              </div>
+              <div className="video">
+                {callAccepted && !callEnded && <video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} />}
+              </div>
+            </div>
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn color="danger" onClick={leaveCall}>End Call</MDBBtn>
+          </MDBModalFooter>
+        </MDBModalContent>
+      </MDBModalDialog>
+    </MDBModal>
+  );
+  
+  
 
-  // Function to toggle sound mute status
-  const toggleSoundMute = () => {
-    setIsSoundMuted(!isSoundMuted);
-  };
-
-  // Function to minimize the modal
-  const minimizeModal = () => {
-    // Add logic to minimize the modal here
-    // You can hide it, minimize it to a corner, or perform any other necessary actions.
-  };
-
-  // Function to close the modal
-  const closeModal = () => {
-    // Add logic to close the modal here
-    setModalShow(false);
-  };
-
-  // Calculate call duration
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setCallDuration(callDuration + 1);
-    }, 1000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [callDuration]);
+  const CallPopupModal = () => (
+    <MDBModal show={showCallPopup} tabIndex="-1">
+      <MDBModalDialog>
+        <MDBModalContent>
+          <MDBModalHeader>
+            <MDBModalTitle>Incoming Call</MDBModalTitle>
+          </MDBModalHeader>
+          <MDBModalBody>
+            <p>You have an incoming call from {caller}.</p>
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn color="danger" onClick={rejectCall}>Reject</MDBBtn>
+            <MDBBtn color="success" onClick={answerCall}>Accept</MDBBtn>
+          </MDBModalFooter>
+        </MDBModalContent>
+      </MDBModalDialog>
+    </MDBModal>
+  );
 
   return (
     <div className="vh-100" style={{ backgroundColor: "#E6E6E6" }}>
       <MDBContainer>
         <MDBRow className="justify-content-center">
           <MDBCol md="9" lg="7" xl="5" className="mt-5">
+            <button onClick={() => setModalShow(true)}>Open Modal</button> {/* Button to open modal */}
+            
+            {/* Modal Structure */}
             <MDBModal
               show={modalShow}
               tabIndex="-1"
@@ -245,40 +222,43 @@ export default function ECommerce() {
               <MDBModalDialog>
                 <Draggable>
                   <MDBModalContent>
-                  <MDBModalBody onClick={(e) => e.stopPropagation()}>
+                    <MDBModalBody onClick={(e) => e.stopPropagation()}>
                       <div className="d-flex justify-content-end">
                         <h4>TEST me.id = {me} </h4>
-                        </div>
-                        <input onChange={(e) => setIdToCall(e.target.value)} value={idToCall} ></input>
-                        <h4>Test ID to call : {idToCall}</h4>
-                        <button style={{backgroundColor: "red"}} onClick={() => callUser(idToCall)}>
-                          call 
-                        </button>
-                        <h1>is receiving a call ? {receivingCall.toString()}</h1>
-                        <h1>is callAccepted ? {callAccepted.toString()}</h1>
-                        {receivingCall && !callAccepted ? (
+                      </div>
+                      <input onChange={(e) => setIdToCall(e.target.value)} value={idToCall} ></input>
+                      <h4>Test ID to call : {idToCall}</h4>
+                      <button style={{backgroundColor: "red"}} onClick={() => callUser(idToCall)}>
+                        call 
+                      </button>
+                      <h1>is receiving a call ? {receivingCall.toString()}</h1>
+                      <h1>is callAccepted ? {callAccepted.toString()}</h1>
+                      {receivingCall && !callAccepted ? (
                         <div className="caller">
-                        <h1 >Call received...</h1>
-                          <button onClick={answerCall} >Answer</button>
+                          <h1>Call received...</h1>
+                          <button onClick={answerCall}>Answer</button>
+                        </div>
+                      ) : null}
+  
+                      <div className="video-container">
+                        <div className="video">
+                          {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+                        </div>
+                        <div className="video">
+                          {callAccepted && !callEnded ? <video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} /> : null}
+                        </div>
                       </div>
-                    ) : null}
-
-                    <div className="video-container">
-                      <div className="video">
-                        {<video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
-                      </div>
-                      <div className="video">
-                        {callAccepted && !callEnded ?
-                        <video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
-                        null}
-                      </div>
-                    </div>
-
                     </MDBModalBody>
                   </MDBModalContent>
                 </Draggable>
               </MDBModalDialog>
             </MDBModal>
+  
+            {/* Call Popup Modal */}
+            <CallPopupModal />
+            
+            {/* Call Answer Modal */}
+            <CallAnswerModal />
           </MDBCol>
         </MDBRow>
       </MDBContainer>
